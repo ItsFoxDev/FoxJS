f07a92bb8ac35447f69c13bc99f49a3f70070c32
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2022-12-01 12:28:10 -0500

    added logging and redir

--- latest.js
+++ latest.js
@@ -1,5 +1,8 @@
+console.log("🦊 Loading FoxJS Functions");
 function ask(q,d){return prompt(q,d)};
 function disp(t){alert(t)};
-function thisURL(){return location.hostname}
+function thisURL(){return location.hostname};
 function hasString(b,s){return (b.includes(s))};
 function hasNoString(b,s){return (!b.includes(s))};
+function redir(u){window.location.href=u};
+console.log("✅ FoxJS loaded");
