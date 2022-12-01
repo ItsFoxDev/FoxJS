6c426146780ac423788641426ccc0a8d6923da9e
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2022-12-01 14:51:09 +0000

    🎉 Added 5 functions and test environment

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-function ask(q,d){prompt(q,d)};
+function ask(q,d){return prompt(q,d)};
+function disp(t){alert(t)};
+function thisURL(){return location.hostname}
+function hasString(b,s){return (b.includes(s))};
+function hasNoString(b,s){return (!b.includes(s))};
