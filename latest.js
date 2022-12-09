a9db67d94aebe2d7e7619b117e01df200c23a469
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2022-12-09 16:15:03 -0500

    🎉 2 new functions

--- latest.js
+++ latest.js
@@ -1,8 +1,25 @@
 console.log("🦊 Loading FoxJS Functions");
+// Simple functions
 function ask(q,d){return prompt(q,d)};
 function disp(t){alert(t)};
 function thisURL(){return location.hostname};
 function hasString(b,s){return (b.includes(s))};
 function hasNoString(b,s){return (!b.includes(s))};
 function redir(u){window.location.href=u};
+// Complex functions
+function getSelected() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+function speak(text,rate){
+  var speak = new SpeechSynthesisUtterance();
+  speak.text = text;
+  speak.rate = rate;
+  window.speechSynthesis.speak(speak);
+}
 console.log("✅ FoxJS loaded");
