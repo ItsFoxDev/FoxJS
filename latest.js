fcfccb7c734bbe7f105d828e3cfe2e48778aa2b3
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2023-02-20 17:48:02 -0500

    🎉 • Added a few more functions

--- latest.js
+++ latest.js
@@ -1,25 +1,14 @@
-console.log("🦊 Loading FoxJS Functions");
+function clog(m,c){console.log("%cFoxJS","color: white; background: " + c + "; padding: 2px 6px; border-radius: 3px; margin-right: 5px;",m);}
+log('Loading simple functions','darkred');
 // Simple functions
 function ask(q,d){return prompt(q,d)};
 function disp(t){alert(t)};
-function thisURL(){return location.hostname};
+function url(){return window.location.href};
 function hasString(b,s){return (b.includes(s))};
 function hasNoString(b,s){return (!b.includes(s))};
 function redir(u){window.location.href=u};
+log('Loading complex functions','darkorange');
 // Complex functions
-function getSelected() {
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
-    return text;
-}
-function speak(text,rate){
-  var speak = new SpeechSynthesisUtterance();
-  speak.text = text;
-  speak.rate = rate;
-  window.speechSynthesis.speak(speak);
-}
-console.log("✅ FoxJS loaded");
+function getSelected() {let fjs = "";if (window.getSelection) {fjs = window.getSelection().toString();} else if (document.selection && document.selection.type != "Control") {fjs = document.selection.createRange().text;}return fs;}
+function speak(t){var fjp = new SpeechSynthesisUtterance();fjp.text = t;window.speechSynthesis.speak(fjp);}
+log('Finished importing all main modules','darkgreen');
